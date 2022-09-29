abc6bdae2b2fc66a55c34c9649cff22065bc1654
suwenxiang <suwenxiang@huawei.com>
2022-09-29 10:24:24 +0800

    Description: Solve the problem of macro definition error IssueNo: https://gitee.com/openharmony/graphic_utils/issues/I5TQU4 Feature or Bugfix: Bugfix Binary Source: No Signed-off-by: suwenxiang <suwenxiang@huawei.com>

--- interfaces/kits/gfx_utils/diagram/imagefilter/filter_blur.h
+++ interfaces/kits/gfx_utils/diagram/imagefilter/filter_blur.h
@@ -58,7 +58,7 @@ public:
             if (isGetRGBAIntegral) {
                 GetRGBAIntegralImage((uint8_t*)img.PixValuePtr(0, 0), width, height, stride);
             }
-#ifdef __ICCARM__
+#ifndef __ICCARM__
 #pragma omp parallel for
 #endif
             for (int32_t y = 0; y < height; y++) {
